69273eb7a648963839e0cce8c195f1ec93ccedfd
Valentin Rangelov <eneragee@gmail.com>
2022-05-14 00:39:21 +0800

    Fixing repository methods

--- src/requests/invitation.request.ts
+++ src/requests/invitation.request.ts
@@ -1,6 +1,7 @@
 import { LinkedInRequest } from '../core/linkedin-request';
 import { GetReceivedInvitationResponse } from '../responses/received-invitations.response.get';
 import { GetSentInvitationResponse } from '../responses/sent-invitations.response.get';
+import { extractProfileId } from '../utils/common-li';
 
 export class InvitationRequest {
   private request: LinkedInRequest;
@@ -10,6 +11,8 @@ export class InvitationRequest {
   }
 
   sendInvitation({ profileId, trackingId, message }: { profileId: string; trackingId: string; message?: string }): Promise<void> {
+    profileId = extractProfileId(profileId);
+    
     const requestPayload = {
       trackingId,
       emberEntityName: 'growth/invitation/norm-invitation',
